d467e39d5cd297e0af820612e48b72b094e24fcd
MadHatter013 <mishakorzh3@gmail.com>
2019-04-03 23:10:11 +0300

    1

--- src/dzLesson12/Task4.java
+++ src/dzLesson12/Task4.java
@@ -6,22 +6,17 @@ public class Task4 {
         StringBuilder stringBuilder1 = new StringBuilder("Code");
         StringBuilder stringBuilder2 = new StringBuilder("Practice");
         StringBuilder stringBuilder3 = new StringBuilder("Pra");
-        stringBuilder = twoMiddleCharacters(stringBuilder);
-        stringBuilder1 = twoMiddleCharacters(stringBuilder1);
-        stringBuilder2 = twoMiddleCharacters(stringBuilder2);
-//        stringBuilder3 = twoMiddleCharacters(stringBuilder3);
 
     }
 
-    public static StringBuilder twoMiddleCharacters(StringBuilder stringBuilder) {
+    public static void twoMiddleCharacters(StringBuilder stringBuilder) {
         int length = stringBuilder.length();
         if (length % 2 != 0) {
             System.out.println("Длина строки - нечетное значение!");
-            System.exit(3);
+            return;
         }
         stringBuilder = stringBuilder.delete(0, (length / 2) - 1);
         stringBuilder = stringBuilder.delete(2, length);
         System.out.println(stringBuilder);
-        return stringBuilder;
     }
 }
